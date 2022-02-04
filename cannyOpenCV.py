a3bb04f0179bc53e374415987a82a877d70cd053
Yusri Caloyloy <yusricaloyloy@gmail.com>
2022-02-04 21:02:36 +0800

    Changed ranged value from contour function to 5,20

--- cannyOpenCV.py
+++ cannyOpenCV.py
@@ -4,42 +4,43 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-# def largestContours(canny, img, img_gray):
+def largestContours(canny, img):
 
-#     contours, hierarchy = cv2.findContours(
-#         canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-#     img_contour = np.copy(img)  # Contours change original image.
-#     # cv2.drawContours(img_contour, contours, -1, (0,255,0), 3) # Draw all - For visualization only
+    # Finding Contour
+    contours, hierarchy = cv.findContours(
+        canny, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    img_contour = np.copy(img)  # Contours change original image.
+    # cv2.drawContours(img_contour, contours, -1, (0,255,0), 3) # Draw all - For visualization only
 
-#     # Contours -  maybe the largest perimeters pinpoint to the leaf?
-#     perimeter = []
-#     max_perim = [0, 0]
-#     i = 0
+    # Contours -  maybe the largest perimeters pinpoint to the leaf?
+    perimeter = []
+    max_perim = [0, 0]
+    i = 0
 
-#     # Find perimeter for each contour i = id of contour
-#     for each_cnt in contours:
-#         prm = cv2.arcLength(each_cnt, False)
-#         perimeter.append([prm, i])
-#         i += 1
+    # Find perimeter for each contour i = id of contour
+    for each_cnt in contours:
+        prm = cv.arcLength(each_cnt, True)
+        perimeter.append([prm, i])
+        i += 1
 
-#     # Sort them
-#     perimeter = quick_sort(perimeter)
+    # Sort them
+    perimeter = quick_sort(perimeter)
 
-#     unified = []
-#     max_index = []
-#     # Draw max contours
-#     for i in range(0, 3):
-#         index = perimeter[i][1]
-#         max_index.append(index)
-#         cv2.drawContours(img_contour, contours, index, (255, 0, 0), 3)
+    unified = []
+    max_index = []
+    # Draw max contours
+    for i in range(5, 20):
+        index = perimeter[i][1]
+        max_index.append(index)
+        # cv.drawContours(img_contour, contours, index, (0, 0, 255), 2)
 
-#     # Get convex hull for max contours and draw them
-#     cont = np.vstack(contours[i] for i in max_index)
-#     hull = cv2.convexHull(cont)
-#     unified.append(hull)
-#     cv2.drawContours(img_contour, unified, -1, (0, 0, 255), 3)
+    # Get convex hull for max contours and draw them
+    cont = np.vstack(contours[i] for i in max_index)
+    hull = cv.convexHull(cont)
+    unified.append(hull)
+    cv.drawContours(img_contour, unified, -1, (0,255, 0), 3)
 
-#     return img_contour, contours, perimeter, hull
+    return img_contour, contours, perimeter, hull
 
 
 def quick_sort(p):
@@ -126,63 +127,32 @@ def grCut(chull, img):
     return img
 
 
-img = cv.imread('testSample1.jpg')  # readimage
-# img = cv.imread('image/bl.jpg') #readimage
-# img = cv.imread('image/q.PNG') #readimage
+
+
+# READ IMAGE
+img = cv.imread('image/v.PNG')
+# img = cv.imread('bl.jpg')
 
 # convert to grayscale
 gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 
-# Smoothing
-# kernel size is none negative & odd numbers only
+# {GAUSSIANBLUR VALUE} kernel size is none negative & odd numbers only
 ks_width = 7
 ks_height = 11
 sigma_x = 5
 sigma_y = 5
 dst = None
-
+#SMOOTHING(Applying GaussianBlur)
 img_blur = cv.GaussianBlur(gray, (ks_width, ks_height), sigma_x, dst, sigma_y)
-# img_blur = cv.blur(gray,(0,0),0)#Smoothing
 
-# Canny(Finding Edge)
-# Noise Reduction, Finding Intensity Gradient of the Image,
-canny = cv.Canny(img_blur, 8, 20, L2gradient=True)
+# CANNY(Finding Edge)
+canny = cv.Canny(img_blur, 8, 30, L2gradient=True)
 
+# FINDING CONTOUR
+# Largest Contour - Not the best segmentation
+img_contour, contours, perimeters, hull = largestContours(canny, img)
 
-# Finding Contour
-contours, hierarchy = cv.findContours(
-    canny, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-img_contour = np.copy(img)  # Contours change original image.
-# cv2.drawContours(img_contour, contours, -1, (0,255,0), 3) # Draw all - For visualization only
-
-# Contours -  maybe the largest perimeters pinpoint to the leaf?
-perimeter = []
-max_perim = [0, 0]
-i = 0
-
-# Find perimeter for each contour i = id of contour
-for each_cnt in contours:
-    prm = cv.arcLength(each_cnt, True)
-    perimeter.append([prm, i])
-    i += 1
-
-# Sort them
-perimeter = quick_sort(perimeter)
-
-unified = []
-max_index = []
-# Draw max contours
-for i in range(8, 30):
-    index = perimeter[i][1]
-    max_index.append(index)
-    cv.drawContours(img_contour, contours, index, (255, 0, 0), 3)
-
-# Get convex hull for max contours and draw them
-cont = np.vstack(contours[i] for i in max_index)
-hull = cv.convexHull(cont)
-unified.append(hull)
-cv.drawContours(img_contour, unified, -1, (0, 0, 255), 3)
-
+#Cutting the contoured nail
 img_grcut = grCut(hull, img)
 # cv2.imwrite('contours_none_image1.jpg', image_copy)
 # cv2.destroyAllWindows()
